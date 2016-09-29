2c46fc0904915c6f84bf19bbbc8a687ed48ed33b
Győző Horváth <gyozo.horvath@inf.elte.hu>
2016-09-29 14:26:46 +0200

    3. gyakorlat: kiegészítés a reg oldalhoz ellenőrzés, hibajelzés

--- gyak03/server.js
+++ gyak03/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
+const indicative = require('indicative');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const app = express();
 nunjucks.configure('views', {
@@ -9,12 +12,19 @@ nunjucks.configure('views', {
 });
 
 // Middlewares
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
   console.log(req.method, req.url);
   next();
 });
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  cookie: { maxAge: 60000 },
+  secret: 'secret code',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(flash());
 
 app.get('/hello/:name', function (req, res) {
   const name = req.params.name;
@@ -32,15 +42,41 @@ app.get('/bgcolor', function (req, res) {
 });
 
 app.get('/reg', function (req, res) {
+  const errors = req.flash('errors');
+  const old_data = req.flash('old_data');
+
   res.render('reg.njk', {
-  });
+    errors,
+    old_data,
+  })
 });
 
 app.post('/reg', function (req, res) {
-  console.log(req.body);
-  const {username} = req.body;
-  res.end('ora vege');
-});
+  // console.log(req.body);
+  const rules = {
+    username  : 'required|alpha_numeric',
+    email     : 'required|email',
+    password  : 'required|min:6|max:30',
+    password_confirm : 'same:password',
+  }
+
+  indicative
+    .validateAll(req.body, rules)
+    .then(function () {
+      // validation passed
+      // console.log('success')
+      res.redirect('/hello/siker');
+      res.end();
+    })
+    .catch(function (errors) {
+      // validation failed
+      console.log(errors)
+      req.flash('errors', errors);
+      req.flash('old_data', req.body);
+      res.redirect('/reg');
+      res.end();
+    });
+})
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
